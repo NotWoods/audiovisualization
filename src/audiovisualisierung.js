ae550d815b3fb3864c2489cf7c0480037ec53c3c
Tiger Oakes <contact@tigeroakes.com>
2020-12-07 23:56:15 -0800

    Remove non-mic input

--- src/audiovisualisierung.js
+++ src/audiovisualisierung.js
@@ -14,7 +14,6 @@ var ctx = null;
 var microphone = null;
 var ctxDraw = null;
 
-var loader;
 var filename;
 var fileChosen = false;
 var hasSetupUserMedia = false;
@@ -30,143 +29,10 @@ if (!window.requestAnimationFrame)
 
 $(function () {
 		"use strict";
-	    loader = new BufferLoader();
-	    initBinCanvas();	
+	    initBinCanvas();
 });
 
-function handleFiles(files) {
-    if(files.length === 0){
-        return;
-    }
-	fileChosen = true;
-    setupAudioNodes();
-	var fileReader  = new FileReader();
-    fileReader.onload = function(){
-         var arrayBuffer = this.result;
-         console.log(arrayBuffer);
-         console.log(arrayBuffer.byteLength);
-		
-		 filename = files[0].name.toString();
-		filename = filename.slice(0, -4);
-		console.log(filename);
-
-		var url = files[0].urn || files[0].name;
-		ID3.loadTags(url, function() {
-			var tags = ID3.getAllTags(url);
-
-//                    console.log(tags.title.toString().length);
-//                    if (tags.title.length > 14) {
-//                        var newTitle = tags.title.substring(0,14);
-//                        newTitle += "...";
-//                        $("#title").html(newTitle);
-//                    }
-//                    else {
-//                        $("#title").html(tags.title);
-//                    }
-			if (tags.title.length > 14 && tags.title.length <= 17) {
-
-				$("#title").css("font-size", "7.5vh");
-				
-			}
-			if (tags.title.length > 17 && tags.title.length <= 20) {
-				
-				$("#title").css("font-size", "6.5vh");
-			}
-			
-			if (tags.title.length > 20) {
-				
-				$("#title").css("font-size", "5vh");
-				
-			}
-			
-			$("#title").html(tags.title);
-			
-			onWindowResize();
-
-			$("#title").css("visibility", "visible");
-
-			$("#artist").html(tags.artist);
-			$("#artist").css("visibility", "visible");
-			$("#album").html(tags.album);
-			$("#album").css("visibility", "visible");
-		  }, {
-			tags: ["title","artist","album","picture"],
-			dataReader: ID3.FileAPIReader(files[0])
-		  });
-		
-     };
-     fileReader.readAsArrayBuffer(files[0]);
-     var url = URL.createObjectURL(files[0]); 
-	
-	var request = new XMLHttpRequest();
-	
-	request.addEventListener("progress", updateProgress);
-	request.addEventListener("load", transferComplete);
-	request.addEventListener("error", transferFailed);
-	request.addEventListener("abort", transferCanceled);
-	
-	request.open('GET', url, true);
-	request.responseType = 'arraybuffer';
-
- 	// When loaded decode the data
-	request.onload = function() {
-		// decode the data
-		context.decodeAudioData(request.response, function(buffer) {
-		// when the audio is decoded play the sound
-		sourceNode.buffer = buffer;
-		sourceNode.start(0);
-		$("#freq, body").addClass("animateHue");
-		//on error
-		}, function(e) {
-			console.log(e);
-		});
-	};
-	request.send();
-	
-	$("button, input").prop("disabled",true);
-}
-
-function playSample() {
-	
-	fileChosen = true;
-    setupAudioNodes();
-	
-	var request = new XMLHttpRequest();
-	
-	request.addEventListener("progress", updateProgress);
-	request.addEventListener("load", transferComplete);
-	request.addEventListener("error", transferFailed);
-	request.addEventListener("abort", transferCanceled);
-	
-	request.open('GET', 'src/Infinite.mp3', true);
-	request.responseType = 'arraybuffer';
-
- 	// When loaded decode the data
-	request.onload = function() {
-		
-		$("#title").html("Infinite");
-		$("#album").html("Infinite");
-		$("#artist").html("Valence");
-		onWindowResize();
-		$("#title, #artist, #album").css("visibility", "visible");
-		
-		// decode the data
-		context.decodeAudioData(request.response, function(buffer) {
-		// when the audio is decoded play the sound
-		sourceNode.buffer = buffer;
-		sourceNode.start(0);
-		$("#freq, body").addClass("animateHue");
-		//on error
-		}, function(e) {
-			console.log(e);
-		});
-	};
-	request.send();
-	
-	$("button, input").prop("disabled",true);
-}
-
-function useMic() 	
+function useMic()
 {
 	"use strict";
 	if (!navigator.mediaDevices.getUserMedia) {
@@ -174,7 +40,7 @@ function useMic()
 		console.log('Your browser does not support microphone input!');
 		return;
  	}
-	
+
 	navigator.mediaDevices.getUserMedia({audio: true, video: false})
 	.then(function(stream) {
 		hasSetupUserMedia = true;
@@ -186,13 +52,15 @@ function useMic()
 		microphone.connect(analyser);
 		//start updating
 		rafID = window.requestAnimationFrame( updateVisualization );
-		
+
 		$("#title").html("Mic");
 		$("#album").html("Input");
 		$("#artist").html("Using");
 		onWindowResize();
 		$("#title, #artist, #album").css("visibility", "visible");
 		$("#freq, body").addClass("animateHue");
+
+		document.getElementById('useMicButton').style.display = 'none'
 	})
 	.catch(function(err) {
 	  /* handle the error */
@@ -241,12 +109,12 @@ function initBinCanvas () {
         c.height = window.innerHeight;
 	//get context from canvas for drawing
 	ctx = c.getContext("2d");
-	
+
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = window.innerHeight;
-	
+
 	window.addEventListener( 'resize', onWindowResize, false );
-	
+
 	//create gradient for the bins
 	var gradient = ctx.createLinearGradient(0, c.height - 300,0,window.innerHeight - 25);
 	gradient.addColorStop(1,'#00f'); //black
@@ -254,7 +122,7 @@ function initBinCanvas () {
 	gradient.addColorStop(0.25,'#f00'); //yellow
 	gradient.addColorStop(0,'#ffff00'); //white
 
-	
+
 	ctx.fillStyle = "#9c0001";
 }
 
@@ -262,12 +130,12 @@ function onWindowResize()
 {
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = window.innerHeight;
-	
+
 	var containerHeight = $("#song_info_wrapper").height();
-	var topVal = $(window).height() / 2 - containerHeight / 2; 
+	var topVal = $(window).height() / 2 - containerHeight / 2;
 	$("#song_info_wrapper").css("top", topVal);
 	console.log(topVal);
-	
+
 	if($(window).width() <= 500) {
 		//TODO: not yet working
 		$("#title").css("font-size", "40px");
@@ -280,7 +148,7 @@ function setupAudioNodes() {
 	// setup a analyser
 	analyser = context.createAnalyser();
 	// create a buffer source node
-	sourceNode = context.createBufferSource();	
+	sourceNode = context.createBufferSource();
 	//connect source to analyser as link
 	sourceNode.connect(analyser);
 	// and connect source to destination
@@ -292,7 +160,7 @@ function setupAudioNodes() {
 
 function reset () {
 	if (typeof sourceNode !== "undefined") {
-		sourceNode.stop(0);		
+		sourceNode.stop(0);
 	}
 	if (typeof microphone !== "undefined") {
 		microphone = null;
@@ -301,7 +169,7 @@ function reset () {
 
 
 function updateVisualization () {
-        
+
 	// get the average, bincount is fftsize / 2
 	if (fileChosen || hasSetupUserMedia) {
 		var array = new Uint8Array(analyser.frequencyBinCount);
@@ -310,7 +178,7 @@ function updateVisualization () {
 		drawBars(array);
 	}
        // setTextAnimation(array);
-    
+
 
 	rafID = window.requestAnimationFrame(updateVisualization);
 }
@@ -325,7 +193,7 @@ function drawBars (array) {
 	var maxBinCount = array.length;
 	//space between bins
 	var space = 3;
-        
+
 	ctx.save();
 
 
@@ -336,7 +204,7 @@ function drawBars (array) {
 	ctx.translate(window.innerWidth, window.innerHeight);
 	ctx.fillStyle = "#fff";
 
-	var bass = Math.floor(array[1]); //1Hz Frequenz 
+	var bass = Math.floor(array[1]); //1Hz Frequenz
 	var radius = 0.45 * $(window).width() <= 450 ? -(bass * 0.25 + 0.45 * $(window).width()) : -(bass * 0.25 + 450);
 
 	var bar_length_factor = 1;
@@ -352,21 +220,21 @@ function drawBars (array) {
 	console.log($(window).width());
 	//go over each bin
 	for ( var i = 0; i < maxBinCount; i++ ){
-		
+
 		var value = array[i];
-		if (value >= threshold) {			
+		if (value >= threshold) {
 			//draw bin
 			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
                         //ctx.fillRect(i * space, c.height, 2, -value);
                         ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
-                        ctx.rotate((180 / 128) * Math.PI/180);   
+                        ctx.rotate((180 / 128) * Math.PI/180);
 		}
-	}  
-        
+	}
+
 	for ( var i = 0; i < maxBinCount; i++ ){
 
 		var value = array[i];
-		if (value >= threshold) {				
+		if (value >= threshold) {
 
 			//draw bin
 			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
@@ -374,12 +242,12 @@ function drawBars (array) {
 						ctx.rotate(-(180 / 128) * Math.PI/180);
 						ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
 		}
-	} 
-        
+	}
+
 	for ( var i = 0; i < maxBinCount; i++ ){
 
 		var value = array[i];
-		if (value >= threshold) {				
+		if (value >= threshold) {
 
 			//draw bin
 			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
@@ -387,28 +255,28 @@ function drawBars (array) {
 						ctx.rotate((180 / 128) * Math.PI/180);
 						ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
 		}
-	} 
-    
+	}
+
 	ctx.restore();
 }
 
 //function setTextAnimation(array)
 //{
-//    var bass = Math.floor(array[1]); //4Hz Frequenz 
-//    
-//    
+//    var bass = Math.floor(array[1]); //4Hz Frequenz
+//
+//
 //    var fontSize = bass * 0.25 + 50;
-//    
+//
 //    ctx.save();
 //    ctx.globalCompositeOperation='destination-over';
 //    ctx.fillStyle = "#fff";
 //    ctx.filter = "blur(16px)";
-//    
+//
 //    var neueFontsize = 70;
 //    if (fontSize > neueFontsize) {
 //        neueFontsize = fontSize;
 //    }
-//    
+//
 //    //ctx.font = neueFontsize.toString() + "px Arial";
 //    if (navigator.userAgent.indexOf("Chrome/53.0.2764.0") > -1) { //besserer Look in Chrome Canary
 //        console.log("Chrome Canary User Agent detected");
@@ -425,5 +293,3 @@ function drawBars (array) {
 //    }
 //    ctx.restore();
 //}
-
-
